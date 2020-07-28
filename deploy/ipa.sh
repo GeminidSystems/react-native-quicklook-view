47c2a2e732c94448958b749b33699cac22d655c5
geoffreyxue <xuegeoff@gmail.com>
2020-07-28 13:48:16 -0500

    ipa.sh still not working

--- deploy/ipa.sh
+++ deploy/ipa.sh
@@ -1,3 +1,5 @@
+react-native bundle --entry-file='index.js' --bundle-output='./ios/main.jsbundle' --dev=false --platform='ios'
+
 #Generate Archive File
 xcodebuild -workspace ios/ReactPOC.xcworkspace -scheme ReactPOC clean archive -configuration release -sdk iphoneos -archivePath dist/Archive.xcarchive -quiet
 
