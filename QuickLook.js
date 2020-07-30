6f7ef4aa1a499d75f8abf702feb4535727ca82c7
geoffreyxue <xuegeoff@gmail.com>
2020-07-30 12:49:47 -0500

    reformatted functionality, looking into other controllers

--- QuickLook.js
+++ QuickLook.js
@@ -1,51 +1,66 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {
     View,
     Text,
-    FlatList
+    FlatList,
+    Dimensions
 } from 'react-native';
 
-import { styles, buttons } from './styles'  
+import {styles} from './styles'  
 
 import {data} from './Data'
 
-import QuickLookView, {FileType, FileSource} from './QuickLookView'
-
-/*
-        fileSource: FileSource.MAIN,
-        url: 'test.jpeg',
-        fileData:
-        fileType:*/
-
-const Item = ({item}) => (
-    <View style = {styles.item}>
-      <Text style = {styles.text}>{item.title}</Text>
-      <QuickLookView
-            style = {styles.quicklook}
-            height = {300}
-            fileSource = {item.fileSource}
-            url = {item.url}
-            fileData = {item.fileData}
-            fileType = {item.fileType}
-            fileID = {parseInt(item.id, 10)}
-            thumbnail = {item.thumbnail}
-            onTap = {() => console.log("Tapped")}
-            onLongPress = {() => console.log("Long Pressed")}
-      />
-    </View>
-);
+import QuickLookView from './QuickLookView'
 
 const renderSeparator = () => {return (<View style = {styles.separator}/>); };  
 
 export class QuickLook {
-    static screen({navigation}) {
-        return (
-              <FlatList contentContainerStyle = {styles.list}
-                data = {data}
-                renderItem = {Item}
-                keyExtractor = {item => item.id}
-                ItemSeparatorComponent = {renderSeparator}
-              />
-          );
-    }
+
+  static previewTitle = "Example"
+
+  static onPressText(item, navigation) {
+    console.log(item);
+    navigation.navigate("QuickLookPreview", {
+      id: item.id,
+      fileSource: item.fileSource,
+      url: item.url,
+      fileData: item.fileData,
+      fileType: item.fileType,
+      title: item.title
+    })
+  }
+
+  static screen({navigation}) {
+      return (
+            <FlatList contentContainerStyle = {styles.list}
+              data = {data}
+              renderItem = {({item}) => (
+                <View style = {styles.item}>
+                  <Text style = {styles.text} onPress = {() => QuickLook.onPressText(item, navigation)}>{item.title}</Text>
+                </View>
+              )}
+              keyExtractor = {item => item.id}
+              ItemSeparatorComponent = {renderSeparator}
+            />
+        );
+  }
+
+  static previewScreen({route, navigation}) {
+    const {id, fileSource, url, fileData, fileType, title} = route.params;
+    return (
+      <View style = {styles.container}>
+        <QuickLookView
+          style = {styles.quicklook}
+          height = {Dimensions.get('window').height}
+          fileSource = {fileSource}
+          url = {url}
+          fileData = {fileData}
+          fileType = {fileType}
+          fileID = {parseInt(id, 10)}
+          onTap = {() => console.log("Tapped")}
+          onLongPress = {() => console.log("Long Pressed")}
+        />
+      </View>
+    );
+  }
 }
--- ios/QuickLookViewManager.m
+++ ios/QuickLookViewManager.m
@@ -13,4 +13,7 @@ RCT_REMAP_VIEW_PROPERTY(url, urlString, NSString)
 RCT_EXPORT_VIEW_PROPERTY(fileData, NSString)
 RCT_EXPORT_VIEW_PROPERTY(fileType, NSString)
 RCT_EXPORT_VIEW_PROPERTY(fileID, NSNumber)
+
+RCT_EXPORT_VIEW_PROPERTY(onTap, RCTDirectEventBlock)
+RCT_EXPORT_VIEW_PROPERTY(onLongPress, RCTDirectEventBlock)
 @end
