66863e20a513c4f5188eddd5282384e4bc6250f2
Karl Gunst <karl.gunst@geminidsystems.com>
2020-08-06 17:56:36 -0500

    made npm module, removed unrelated code, cleanup

--- deploy/ipa.sh
+++
@@ -1,7 +0,0 @@
-react-native bundle --entry-file='index.js' --bundle-output='./ios/main.jsbundle' --dev=false --platform='ios'
-
-#Generate Archive File
-xcodebuild -workspace ios/ReactPOC.xcworkspace -scheme ReactPOC clean archive -configuration release -sdk iphoneos -archivePath dist/Archive.xcarchive -quiet
-
-#Generate IPA w/
-xcodebuild -exportArchive -archivePath  dist/Archive.xcarchive -exportOptionsPlist  exportOptions.plist -exportPath  dist -quiet
--- ios/QuickLookViewManager.m
+++ ios/QuickLookViewManager.m
@@ -5,7 +5,7 @@
 //  Created by Geoffrey Xue on 7/22/20.
 //
 
-#import "ReactPOC-Bridging-Header.h"
+#import "QuickLookView-Bridging-Header.h"
 
 @interface RCT_EXTERN_MODULE(QuickLookViewManager, RCTViewManager)
 RCT_EXPORT_VIEW_PROPERTY(fileSource, NSNumber)
