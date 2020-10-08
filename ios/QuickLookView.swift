6706dbbb04c837aa21e7214bbeb68f0d1d605de1
Karl Gunst <karl.gunst@geminidsystems.com>
2020-10-08 11:33:58 -0500

    release 1.0.3, readme updates

--- ios/QuickLookView.swift
+++ ios/QuickLookView.swift
@@ -2,7 +2,7 @@
 //  QuickLook.swift
 //  POCApp
 //
-//  Created by Geoffrey Xue on 8/14/20.
+//  Created by Geoffrey Xue on 7/21/20.
 //
 
 import Foundation
@@ -11,17 +11,30 @@ import QuickLook
 
 class QuickLookView : UIView, QLPreviewControllerDataSource, QLPreviewControllerDelegate {
 
-  @objc var onTap: RCTDirectEventBlock?
-  @objc var onPress: RCTDirectEventBlock?
-  @objc var onFinishedLoading: RCTDirectEventBlock?
   private var previewView: UIView?
   private var controller: QLPreviewController?
-  private var previewURL: URL = URL(fileURLWithPath: "")
+    private var previewURL: URL? = nil //URL = Bundle.main.url(forResource: "noURL.png", withExtension: nil)!
+  @objc var fileSource: NSNumber = -1 {
+     didSet {handleUpdate()}
+  }
+  @objc var urlString: NSString = "" {
+    didSet {handleUpdate()}
+  }
+  @objc var fileData: NSString = "" {
+     didSet {handleUpdate()}
+  }
+  @objc var fileType: NSString = "" {
+    didSet {handleUpdate()}
+  }
+  @objc var fileID: NSString = "" {
+    didSet {handleUpdate()}
+  }
 
   override init(frame: CGRect) {
+
     super.init(frame: frame)
     print("Initing QuickLookView from Swift")
-    
+
 
     previewView = UIView()
     controller = QLPreviewController()
@@ -32,80 +45,69 @@ class QuickLookView : UIView, QLPreviewControllerDataSource, QLPreviewController
     controller!.view.backgroundColor = UIColor.black
     controller!.automaticallyAdjustsScrollViewInsets = false
     addSubview(previewView!)
-    
-    self.addGestureRecognizer(UITapGestureRecognizer(
-      target: self,
-      action: #selector(sendTap(_:))));
-    
-    self.addGestureRecognizer(UILongPressGestureRecognizer(
-      target: self,
-      action: #selector(sendPress(_:))));
+
   }
-  
+
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
   }
-  
-  @objc
-   func sendTap(_ gesture: UITapGestureRecognizer) {
-     if gesture.state == .ended {
-       if onTap != nil {
-         onTap!(["view": self])
-       }
-     }
-   }
-   
-   @objc
-   func sendPress(_ gesture: UILongPressGestureRecognizer) {
-     if gesture.state == .began {
-       print("Pressed")
-       if onPress != nil {
-         onPress!(["view": self])
-       }
-     }
-   }
 
   func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
     return 1
   }
-  
-  func generatePreview(src: NSString, fileType: NSString?, fileID: NSString) {
-    // fileID and src are required to generate a preview of a file
-    if (fileID == "" || src == "") {
-      self.handleError()
+
+  func handleUpdate() {
+    //print("handled Update")
+    //print(fileSource)
+    //print(urlString)
+    //print(fileData)
+    //print(fileType)
+    //print(fileID)
+
+    if (fileSource == -1) {
       return
     }
-    
-    // If the file is base64, a fileType is required
-    if (!src.contains("https://") && !src.contains("file:///") && !src.contains("http://")) {
-      if (fileType == nil) {
-        self.handleError()
-        return
+
+    // TODO: Add try catch with this enum
+    let source: FileSource = FileSource.init(rawValue: Int(truncating: fileSource))!
+    switch (source) {
+    case .Local:
+      if (!(urlString as String).isEmpty && !(fileID as String).isEmpty) {
+        generatePreviewView(fileSource: source, urlString: urlString, fileData: fileData, fileType: fileType, fileID: fileID)
       }
+      break
+    case .Downloadable:
+      if (!(urlString as String).isEmpty && !(fileID as String).isEmpty) {
+        generatePreviewView(fileSource: source, urlString: urlString, fileData: fileData, fileType: fileType, fileID: fileID)
+      }
+      break
+    case .Main:
+      if (!(urlString as String).isEmpty) {
+        generatePreviewView(fileSource: source, urlString: urlString, fileData: fileData, fileType: fileType, fileID: fileID)
+      }
+      break
+    case .Base64:
+      if (!(fileData as String).isEmpty && !(fileType as String).isEmpty && !(fileID as String).isEmpty) {
+        generatePreviewView(fileSource: source, urlString: urlString, fileData: fileData, fileType: fileType, fileID: fileID)
+      }
+      break
     }
-    
-    Util.getFile(fileID: fileID, src: src, fileType: fileType) { (success: Bool, fileLocation: URL?) in
+  }
+
+  func generatePreviewView(fileSource: FileSource, urlString: NSString?, fileData: NSString?, fileType: NSString?, fileID: NSString?) {
+    Util.getFile(fileSource: fileSource,
+                 urlString: urlString,
+                 fileData: fileData, fileType: fileType,
+                 fileID: fileID) { (success: Bool, fileLocation: URL?) in
+
       if (success) {
         self.previewURL = fileLocation!
         self.controller!.refreshCurrentPreviewItem()
       }
-      else {
-        self.handleError()
-      }
-      if self.onFinishedLoading != nil {
-        self.onFinishedLoading!(["view": self])
-      }
-     }
+    }
   }
-  
-  func handleError() {
-    print("Error handler called")
-    //self.previewURL = Bundle.main.url(forResource: "noURL.png", withExtension: nil)!
-    self.previewURL = URL(fileURLWithPath: "error")
-    self.controller!.refreshCurrentPreviewItem()
-  }
-  
+
   func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
-    return previewURL as QLPreviewItem
+    return previewURL! as QLPreviewItem
   }
 }
